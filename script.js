15ba86c8112a2a1508a4faa8f53330af017cea9c
AlexMarch95 <bigbenfaq@mail.ru>
2024-02-01 23:42:11 +0300

    feat: creat checkpoint save

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 
-const audio = document.getElementById('music')
-const stopBtn = document.getElementById('button.pause')
+const audio1 = document.getElementById('music1')
+const audio2 = document.getElementById('music2')
 let isPlay = false;
+
 function playAudio() {
     if (isPlay === false) {
-        audio.play();
+        audio1.play();
         isPlay = true;
+        document.querySelector('.button.play-pause').classList.toggle('open')
     } else {
-        audio.pause();
+        audio1.pause();
         isPlay = false
+        document.querySelector('.button.play-pause').classList.toggle('open')
     }
     
+}
+function playNext() {
+ if (audio1.play()) {
+    audio1.pause()
+    audio1.currentTime = 0;
+    audio2.play()
+    document.querySelector('.button.play-pause').classList.toggle('open')
+ } 
 }
